a1c5215bcb4059848a88de31713d4cd4585ce184
Nathan <nathan.le@berkeley.edu>
2017-11-14 17:54:35 -0800

    add index method

--- Java Shazam/Main.java
+++ Java Shazam/Main.java
@@ -4,13 +4,27 @@
 public final class Main {
 
 
-     private static final int FUZ_FACTOR = 2;
+     private static final int hash_factor = 2;
+     public final int[] bins = new int[] { 40, 80, 120, 180, 300 };
 
      public static void main(String[] args) {
-	
+
+
+
      }
 
-
+     /*** 
+     Used to find which frequency range we are in
+     Arguments:
+     freq = input frequency
+     Output = index within bins
+     ***/
+     public int getIndex(int freq) {
+         int i = 0;
+         while (bin[i] < freq)
+             i++;
+         return i;
+     }
      /*** 
      Use to Hash the selected frequencies in a given window. 
      Arguments:
@@ -22,6 +36,6 @@ public final class Main {
          long p2 = Long.parseLong(p[1]);
          long p3 = Long.parseLong(p[2]);
          long p4 = Long.parseLong(p[3]);
-         return  (p4-(p4%FUZ_FACTOR)) * 100000000 + (p3-(p3%FUZ_FACTOR)) * 100000 + (p2-(p2%	FUZ_FACTOR)) * 100 + (p1-(p1%FUZ_FACTOR));
+         return  (p4-(p4%hash_factor)) * 100000000 + (p3-(p3%hash_factor)) * 100000 + (p2-(p2%	hash_factor)) * 100 + (p1-(p1%hash_factor));
      }
 }
